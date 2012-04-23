1eef5c8159d5d6c5ee2544e15966e63524f59f60
Dennis Kuczynski <dennis.kuczynski@gmail.com>
2012-04-23 18:13:44 -0400

    Added a few capybara tests for the front-end

--- spec/lib/beanstalkd_view/server_spec.rb
+++ spec/lib/beanstalkd_view/server_spec.rb
@@ -31,12 +31,43 @@ describe BeanstalkdView::Server, :type => :request do
     it "show be able to add a job on the overview page", :js => true do
       visit '/'
       form = find('#add_job_form')
+      form.fill_in 'form_tube_name', :with => 'test.tube'
+      form.fill_in 'form_job_body', :with => '{"id": 1, "name": "Bob"}'
       form.click_link('Add Job')
       body.should have_content "Add new job?"
       click_link "confirm_add_job_btn"
       body.should have_content "Added job "
     end
     
+    it "show be able to click on the test.tube link (created by the last test)", :js => true do
+      visit '/'
+      click_link('test.tube')
+      body.should have_content "test.tube"
+    end
+    
+=begin
+    # Current beanstalk-client lib doesn't support pause_tube action
+    it "show be able to pause a tube", :js => true do
+      visit '/tube/test.tube'
+      fill_in 'delay', :with => 1
+      click_button "Pause"
+      body.should have_content "Paused test.tube"
+    end
+=end
+
+    it "show be able to kick a tube", :js => true do
+      visit '/tube/test.tube'
+      fill_in 'bound', :with => 1
+      click_button "Kick"
+      body.should have_content "Kicked test.tube"
+    end
+    
+    it "show be able to peek_ready a tube", :js => true do
+      visit '/tube/test.tube'
+      click_button "peek_ready_btn"
+      body.should have_content "Job id:"
+    end
+   
   end
   
   describe "with out beanstalkd daemon running" do
