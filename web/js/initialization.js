6aedcdd9993224f4135f1836a40a07b99eec3308
Dennis Kuczynski <dennis.kuczynski@gmail.com>
2015-05-10 17:53:01 -0400

    wip

--- web/js/initialization.js
+++ web/js/initialization.js
@@ -4,12 +4,6 @@
     //Enable Tooltips
     $("[rel=tooltip]").tooltip();
 
-    if ($('#form_tube_name').length > 0) {
-      //Setup Typeahead For Tube Names on Add Job Form
-      var tubeNames = $('#form_tube_name').data('tubes');
-      $('#form_tube_name').typeahead({source: tubeNames});
-    }
-
     //Update Underscore delimiters <% %> to Handlebar-style {{ }}
     _.templateSettings = {
       interpolate: /\{\{\=(.+?)\}\}/g,
